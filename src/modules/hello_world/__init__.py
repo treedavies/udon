0b45b1778042a9bbafffb00465a27bb3bc2c5eb2
Tree Davies <tree@darkphysics.net>
2026-05-29 04:59:08 -0700

    Add modules directory and hello_world module

---
+++ src/modules/hello_world/__init__.py
@@ -0,0 +1,3 @@
+from .module import run
+__version__ = "1.0.0"
+
---
+++ src/modules/hello_world/module.py
@@ -0,0 +1,11 @@
+
+import json
+
+def run():
+	rc = "0".encode()
+	data = "Hello, World".encode()
+	error = "null".encode()
+	return (rc, data, error)
+
+version = "1.0"
+
