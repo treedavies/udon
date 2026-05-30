71f3d9dd82dd34e25cdc42a8d03924d540505585
Tree Davies <tree@darkphysics.net>
2026-05-29 19:20:12 -0700

    fix modules dir and files

--- src/modules/hello_world/module.py
+++ src/modules/hello_world/module.py
@@ -1,11 +1,9 @@
-
-import json
-
-def run():
-	rc = "0".encode()
-	data = "Hello, World".encode()
-	error = "null".encode()
-	return (rc, data, error)
-
 version = "1.0"
+
+class module:
+	def run(self):
+		rc = "0".encode()
+		data = "Hello, World".encode()
+		error = "null".encode()
+		return (rc, data, error)
 
