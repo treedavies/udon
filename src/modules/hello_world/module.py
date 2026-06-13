39533484391621bacc0d91dff81aba3c5fec9a87
Tree Davies <tree@darkphysics.net>
2026-06-13 10:48:20 -0700

    tweaks / cleanup

--- src/modules/hello_world/module.py
+++ src/modules/hello_world/module.py
@@ -5,22 +5,20 @@ class module:
 	def __init__(self, args=None):
 		self.module_args = args
 
-	def run(self):
+	def run(self) -> ():
 		if type(dict) == type(self.module_args):
-			rc = "1".encode()
-			data = f"null".encode()
-			error = f"hello_world(): rtn() arg != dict".encode()
-			return (rc, data, error)
+			return ("1".encode(),
+					f"null".encode(),
+					f"hello_world(): rtn() arg != dict".encode())
 
 		try:
 			if "name" in self.module_args.keys():
 				name = self.module_args["name"]
 				data = f"Hello, {name}!"
 		except Exception as e:
-			rc = "1".encode()
-			data = f"null".encode()
-			error = f"hello_world(): rtn {e}".encode()
-			return (rc, data, error)
+			return ("1".encode(),
+					f"null".encode(),
+					f"hello_world(): rtn() - {e}".encode())
 
 		return ("0".encode(), data.encode(), "null".encode())
 
